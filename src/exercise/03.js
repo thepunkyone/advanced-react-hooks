fe1fa322c25ebe7101410b49d69b9d37ed4a0130
Whitney Bluzma <whitney@accommodationforstudents.com>
2023-03-30 13:00:36 +0100

    feat: useContext: useCount consumer hook with invalid use handling

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -11,14 +11,25 @@ function CountProvider(props) {
   return <CountContext.Provider value={value} {...props} />
 }
 
+function useCount() {
+  const context = React.useContext(CountContext)
+
+  if (!context)
+    throw new Error(
+      'useCount may only be used from within a (child of a) CountProvider',
+    )
+
+  return context
+}
+
 function CountDisplay() {
-  const [count] = React.useContext(CountContext)
+  const [count] = useCount()
 
   return <div>{`The current count is ${count}`}</div>
 }
 
 function Counter() {
-  const [, setCount] = React.useContext(CountContext)
+  const [, setCount] = useCount()
   const increment = () => setCount(c => c + 1)
   return <button onClick={increment}>Increment count</button>
 }
